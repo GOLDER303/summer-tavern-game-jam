8ba8c9775e9ef2fef6b8a23afe4d423b535e8158
GOLDER303 <szymonstepien@interia.pl>
2023-07-15 18:33:28 +0200

    Add player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,14 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 moveVector = Vector2.zero;
     private Rigidbody2D playerRigidBody;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -23,6 +27,16 @@ public class PlayerController : MonoBehaviour
     private void OnMove(InputValue inputValue)
     {
         moveVector = inputValue.Get<Vector2>();
+        animator.SetBool("isMoving", moveVector != Vector2.zero);
+
+        if (moveVector.x > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (moveVector.x < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 
     private void OnInteract()
