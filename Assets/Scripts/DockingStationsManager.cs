a2839fab23fe1dc2704c31e5e33222c978e3db52
GOLDER303 <szymonstepien@interia.pl>
2023-07-17 09:42:04 +0200

    Create docking stations manager

---
+++ Assets/Scripts/DockingStationsManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DockingStationsManager : MonoBehaviour
+{
+    [SerializeField] private Vector3[] dockingStationsPositions;
+
+    private HashSet<Vector3> occupiedDockingStations = new HashSet<Vector3>();
+
+    public Vector3 GetFreeDockingStation()
+    {
+        if (occupiedDockingStations.Count < dockingStationsPositions.Length)
+        {
+            foreach (Vector3 dockingStationPosition in dockingStationsPositions)
+            {
+                if (!occupiedDockingStations.Contains(dockingStationPosition))
+                {
+                    occupiedDockingStations.Add(dockingStationPosition);
+                    return dockingStationPosition;
+                }
+            }
+        }
+
+        throw new System.Exception("No free docking station");
+    }
+
+    public bool IsFreeStationAvailable()
+    {
+        return occupiedDockingStations.Count < dockingStationsPositions.Length;
+    }
+}
